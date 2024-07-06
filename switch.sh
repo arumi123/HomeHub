06f777782040688257fff51f1a9967fc33352d25
arumi123 <kimusyunapple@gmail.com>
2024-07-06 21:42:42 +0900

    switch.shの作成

--- switch.sh
+++ switch.sh
@@ -1,29 +1,7 @@
-#!/bin/bash
-
-# Get コマンドの処理
-if [ "$1" = "Get" ]; then
-    case "$3" in
-        "On")
-            if [ $? = 0 ]; then
-                echo 1
-            else
-                echo 0
-            fi
-            ;;
-    esac
-    exit 0
-fi
-
-# Set コマンドの処理
-if [ "$1" = "Set" ]; then
-    case "$3" in
-        "On")
-            if [ "$4" = "1" ]; then
+ if [ "$4" = "1" ]; then
                 python3 irrp.py -p -g17 -f codes light:on
+                exit 0
             else
                 python3 irrp.py -p -g17 -f codes light:on
                 exit 0
             fi
-            ;;
-    esac
-fi
